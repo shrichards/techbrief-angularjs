b5bd2a49791e6902ec60089ea22fac2e997bfba7
Seth Richards <seth@shrichards.com>
2013-03-30 22:15:22 -0400

    added controller, now holds data

---
+++ js/controllers.js
@@ -0,0 +1,12 @@
+eatApp.controller("LunchPlaceListCtrl", function($scope){
+	
+	$scope.lunchPlaces = [
+		{ name : 'Sudbury Farms'},
+		{ name : 'Lavender Asian Cuisine & Bar'},
+		{ name : 'Sudbury Pizza'},
+		{ name : 'Sudbury Coffee Works'},
+		{ name : 'Rossini\'s'},
+		{ name : 'Subway'}
+	];
+	
+});
