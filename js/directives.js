4bcf5764b95c710c092197ef3e7fdb54b9f8f6c4
Seth Richards <seth@shrichards.com>
2013-03-31 00:02:37 -0400

    fixed remove by adding '&' to isolate scope

--- js/directives.js
+++ js/directives.js
@@ -2,10 +2,11 @@ eatApp.directive('placeEntry', function(){
 	return {
 		restrict: 'E',
 		scope : {
-			place:'='
+			place:'=',
+			remove:'&'
 		},
 		template: 	'<span>' +
-				   		'<button class="btn btn-danger" data-ng-show={{place.userAdded}} data-ng-click="removePlace(place)">' +
+				   		'<button class="btn btn-danger" data-ng-show={{place.userAdded}} data-ng-click="remove()">' +
 							'<i class="icon-minus icon-white"></i>' +
 						'</button>' +
 						'{{place.name}}' +
