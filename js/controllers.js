69fac53a9025362b9898db1ff14dbc3f84ad120b
Seth Richards <seth@shrichards.com>
2013-03-31 21:42:12 -0400

    refactored location service to use foursquare api

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,19 @@
-eatApp.controller("LunchPlaceListCtrl", function($scope, LunchPlacesService){
+eatApp.controller("LunchPlaceListCtrl", function($scope, $log, $timeout, LunchPlacesService){
 	
-	$scope.getLunchPlaces = function(){
-		return LunchPlacesService.lunchPlaces;
-	}
+	$scope.nearLocation = "Sudbury, MA";
+
+	$scope.lunchPlaces = LunchPlacesService.lunchPlaces;
+
+	var timer=false;
+	$scope.$watch('nearLocation', function(){
+		if(timer){
+			$timeout.cancel(timer)
+		}  
+		timer= $timeout(function(){
+			$log.log("getting places near " + $scope.nearLocation);
+			LunchPlacesService.loadLunchPlaces($scope.nearLocation);
+		},500)
+	});
 
 	$scope.addNewPlace = function(){
 		if(!!$scope.newPlaceName && $scope.newPlaceName != ""){
@@ -10,5 +21,10 @@ eatApp.controller("LunchPlaceListCtrl", function($scope, LunchPlacesService){
 			LunchPlacesService.addPlace(place);
 			$scope.newPlaceName = "";
 		}
+	}
+
+	$scope.loadLunchPlaces = function(nearLocation){
+		$log.log("getting places near " + nearLocation);
+		LunchPlacesService.loadLunchPlaces(nearLocation);
 	}
 });
--- js/services.js
+++ js/services.js
@@ -1,17 +1,39 @@
-eatApp.service('LunchPlacesService', function(){
-	
-	this.lunchPlaces = [
-		{ name : 'Sudbury Farms'},
-		{ name : 'Lavender Asian Cuisine & Bar'},
-		{ name : 'Sudbury Pizza'},
-		{ name : 'Sudbury Coffee Works'},
-		{ name : 'Rossini\'s'},
-		{ name : 'Subway'}
-	];
+eatApp.service('LunchPlacesService', function($resource){
 
-	this.addPlace = function(newPlace)
-	{
+	
+	this.foursquarePlacesResource = $resource(
+			'https://api.foursquare.com/v2/venues/explore?near=:near&section=:section&client_id=:clientId&client_secret=:clientSecret&v=:versionDate',
+			{
+				near:'Sudbury, MA', 
+				section:'food', 
+				clientId:apiKeys.foursquare.clientId, 
+				clientSecret:apiKeys.foursquare.clientSecret,
+				versionDate:'20130401'
+			},
+			{
+				getLunchPlaces : { method:'GET' }
+			}
+		);
+
+	
+	this.lunchPlaces = [];
+
+	this.loadLunchPlaces = function(location){
+		// why don't we assign this to a new empty array?
+		this.lunchPlaces.length = 0;
+		var myService = this;
+		var rawFoursquarePlaces  = this.foursquarePlacesResource.getLunchPlaces({near:location}, function(){
+			var places = _.map(rawFoursquarePlaces.response.groups[0].items, function(item){ return {name: item.venue.name}});	
+			_.each(places, function(p){ myService._addPlace(p)});
+		});
+	}
+
+	this.addPlace = function(newPlace){
 		newPlace.userAdded = true;
+		this._addPlace(newPlace);
+	};
+
+	this._addPlace = function(newPlace)	{
 		if(!_.some(this.lunchPlaces, function(p){ return p.name == newPlace.name; })){
 			this.lunchPlaces[this.lunchPlaces.length] = newPlace;
 		}
@@ -19,6 +41,17 @@ eatApp.service('LunchPlacesService', function(){
 
 	this.removePlace = function(remove){
 		this.lunchPlaces = _.reject(this.lunchPlaces, function(p){ return p === remove; });
+		var removeIndex = -1;
+		for(var i = 0; i < this.lunchPlaces.length; i++){
+			if(this.lunchPlaces[i].userAdded && this.lunchPlaces[i].name == remove.name){
+				removeIndex = i;
+				break;
+			}
+		}
+
+		if(removeIndex > -1){
+			this.lunchPlaces.splice(removeIndex, 1);
+		}
 	};
 
 	this.getLastUserAddedPlace = function(){
